d38af64e975b12137b416f6e48c252a0d54503f7
Graeme Power <gjpower@tcd.ie>
2015-07-15 00:56:45 +0100

    Basic configuration with them

---
+++ init.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(frame-background-mode (quote dark))
+ '(global-linum-mode t)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil))
+
+
+;; Graeme custom options
+(tool-bar-mode -1)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'solarized t)
